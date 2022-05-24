913b6b7b44c04d7115e6f7c3349a748a4cc7bccc
lmusa <lmusa@users.noreply.github.com>
2022-05-24 10:08:52 -0400

    Implement polynomial interpolation
    Signed-off-by: lmusa <lmusa@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 
-all: linear_congruential_random_generator gauss_elimination
+all: linear_congruential_random_generator gauss_elimination poly_interpolation
 
 test: all run_all_tests
 
@@ -10,9 +10,13 @@ linear_congruential_random_generator:
 gauss_elimination:
 	$(CC) ./tests/test_gauss_elimination.c ./src/gauss_elimination.c -lm -Wall -std=c99 -I./src -o test_gauss_elimination.out
 
+poly_interpolation:
+	$(CC) ./tests/test_poly_interpolation.c ./src/poly_interpolation.c -lm -Wall -std=c99 -I./src -o test_poly_interpolation.out
+
 run_all_tests:
 	./test_linear_congruential_random_generator.out
 	./test_gauss_elimination.out
+	./test_poly_interpolation.out
 
 clean:
 	rm -rf test_*.out
---
+++ src/poly_interpolation.h
@@ -0,0 +1,41 @@
+/* This file is part of the 1chipML library. */
+#ifndef _BASE_LIB_
+#define _BASE_LIB_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#endif
+
+/*
+ * The definition below can take the values "float" or "double"
+ * and defines the precision of the variables used in the polynomial
+ * interpolation and extrapolation method. 
+ */
+
+#define poly_real double
+
+/* Functions are declared below */
+
+/*
+ * This function implements the polynomial interpolation or extrapolation 
+ * using Neville's algorithm. Polynomial approach is only suitable for small 
+ * amount of measured data.
+ * Input:
+ *	x_a: Measured x values. Those value has to be ordered.
+ *	y_a: Measured y values pairing with x values.
+ *	n: The number of measured data.
+ *	x: The x value of the data to be interpolated/extrapolated.
+ *	y: Return the y value of the data to be interpolcated/extrapolated.
+ *	error: Return the error estimation.
+ * Return: 
+ *	0 as success, and -1 as error.	
+ */
+int poly_interpolation(poly_real x_a[], 
+												poly_real y_a[], 
+												unsigned int n, 
+												poly_real x, 
+												poly_real* y, 
+												poly_real* error);
+
+/* -- End of file -- */
