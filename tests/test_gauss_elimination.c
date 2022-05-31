cba437473430486ae5731a7569f0dfb4b186fb49
jmsellier <jeanmichel.sellier@gmail.com>
2022-05-31 15:06:34 -0400

    one more minor bug fix - related to Arduino IDE
    Signed-off-by: jmsellier <jeanmichel.sellier@gmail.com>

--- tests/test_gauss_elimination.c
+++ tests/test_gauss_elimination.c
@@ -1,7 +1,7 @@
 /* ************************************************* *
  * Author        : Jean Michel Sellier               *
  * Created       : 08 Feb 2022, Montreal, QC, Canada *
- * Last modified : 14 Feb 2022, Montreal, QC, Canada *
+ * Last modified : 31 May 2022, Montreal, QC, Canada *
  * ************************************************* */
 
 #include<stdio.h>
@@ -14,9 +14,16 @@ int main(void){
  /* Variables and pointers declarations */
  int i;
  int N;
- gauss_real A[2][2];
- gauss_real B[2];
+ gauss_real **A;//[2][2];
+ gauss_real *B;//[2];
  gauss_real *sol; /* pointer towards the solution */
+
+ /* dynamical memory allocation */
+ A=(gauss_real **)malloc(4*sizeof(gauss_real*));
+ for(i=0;i<2;i++){
+  A[i]=(gauss_real *)malloc(2*sizeof(gauss_real));
+ }
+ B=(gauss_real *)malloc(2*sizeof(gauss_real));
 
  /* The following parameters define a system of 2 linear equations A*X=B */
  N=2;
@@ -27,6 +34,11 @@ int main(void){
 
  /* Apply gauss elimination method to solve the system */
  sol=gauss_elimination(N,A,B);
+
+ /* free memory */
+ for(i=0;i<2;i++) free(A[i]);
+ free(A);
+ free(B);
 
  /* Print solution on screen */
  for(i=0;i<N;i++) printf("sol[%d] = %0.3f\n",i,sol[i]);
