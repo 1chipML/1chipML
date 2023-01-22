8f4a7f6f466f09f96a3e28164801e83bdba58567
nicholaslegrand102 <56268673+nicholaslegrand102@users.noreply.github.com>
2023-01-22 10:52:46 -0500

    Formatted source code under the LLVM format (#29)
    Signed-off-by: nicholaslegrand102 <56268673+nicholaslegrand102@users.noreply.github.com>

--- tests/test_linear_congruential_random_generator.c
+++ tests/test_linear_congruential_random_generator.c
@@ -4,21 +4,21 @@
  * Last modified : 14 Feb 2022, Montreal, QC, Canada *
  * ************************************************* */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "../src/1chipml.h"
 
-int main(void){
- /* Variables and pointers declarations */
- int i,n=100;
+int main(void) {
+  /* Variables and pointers declarations */
+  int i, n = 100;
 
- printf("linear congruential random generator\n");
- for(i=0;i<n;i++) printf("%1.3f ",linear_congruential_random_generator());
- printf("\n");
- return(0);
+  printf("linear congruential random generator\n");
+  for (i = 0; i < n; i++)
+    printf("%1.3f ", linear_congruential_random_generator());
+  printf("\n");
+  return (0);
 }
 
 /* -- End of file -- */
-
