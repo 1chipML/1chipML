a970f79201c1456643fd8c8729e3e491b6533aef
felix642 <60141446+felix642@users.noreply.github.com>
2023-02-03 15:22:54 -0500

    Formatted source code under the LLVM format (#29) (#13)
    Signed-off-by: nicholaslegrand102 <56268673+nicholaslegrand102@users.noreply.github.com>
    Co-authored-by: nicholaslegrand102 <56268673+nicholaslegrand102@users.noreply.github.com>
    Signed-off-by: Florence Cloutier <florencecloutier@Florences-MBP.ht.home>

--- src/linear_congruential_random_generator.c
+++ src/linear_congruential_random_generator.c
@@ -3,22 +3,21 @@
 #include "linear_congruential_random_generator.h"
 
 /* The routines below implements the linear congruential random
-   number generator. In particular, the function "set_linear_congruential_generator_seed"
-   sets the seed of the random generator, while the function "linear_congruential_random_generator"
+   number generator. In particular, the function
+   "set_linear_congruential_generator_seed" sets the seed of the random
+   generator, while the function "linear_congruential_random_generator"
    returns a pseudo-random real number in the range [0, 1.0]. */
 
 /* Variables and pointers declarations */
-int ISEED=16.; /* Default value in case the user does not specify the seed */
+int ISEED = 16.; /* Default value in case the user does not specify the seed */
 
 /* Set the seed for the random number generator */
-void set_linear_congruential_generator_seed(int num){
- ISEED=num;
-}
+void set_linear_congruential_generator_seed(int num) { ISEED = num; }
 
 /* Linear congruential random number generator */
-lcrg_real linear_congruential_random_generator(void){
- ISEED=fmod(75.*ISEED+74.,65537.);
- return(ISEED/65537.);
+lcrg_real linear_congruential_random_generator(void) {
+  ISEED = fmod(75. * ISEED + 74., 65537.);
+  return (ISEED / 65537.);
 }
 
 /* -- End of file -- */
