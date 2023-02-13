29fe96e6edf47a5081788ca5a3d46781edb661ec
Paul-Andre Bisson <paul-andre.bisson@polymtl.ca>
2023-02-13 10:18:48 -0500

    Moved lut
    Signed-off-by: Paul-Andre Bisson <paul-andre.bisson@polymtl.ca>

--- src/fast_sincos.c
+++ src/fast_sincos.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "fast_sincos.h"
 //#include <time.h>
-#include <inttypes.h>
+
 
 static fast_sincos_real fastSinChebyshev(const fast_sincos_real angleRadians, const int degree);
 static fast_sincos_real fastCosChebyshev(const fast_sincos_real angleRadians, const int degree);
@@ -11,26 +11,6 @@ static double lookupSinInterpolate(const double x);
 static double lookupCos(const double x);
 static double lookupSin(const double x);
 
-
-const uint16_t sineTable[129] = {
-    0,  804, 1608, 2412, 3216, 4019, 4821, 5623,
- 6424, 7223, 8022, 8820, 9616,10411,11204,11996,
-12785,13573,14359,15142,15924,16703,17479,18253,
-19024,19792,20557,21319,22078,22834,23586,24334,
-25079,25820,26557,27291,28020,28745,29465,30181,
-30893,31600,32302,32999,33692,34379,35061,35738,
-36409,37075,37736,38390,39039,39682,40319,40950,
-41575,42194,42806,43411,44011,44603,45189,45768,
-46340,46905,47464,48014,48558,49095,49624,50145,
-50659,51166,51664,52155,52638,53113,53580,54039,
-54490,54933,55367,55794,56211,56620,57021,57413,
-57797,58171,58537,58895,59243,59582,59913,60234,
-60546,60850,61144,61429,61704,61970,62227,62475,
-62713,62942,63161,63371,63571,63762,63943,64114,
-64276,64428,64570,64703,64826,64939,65042,65136,
-65219,65293,65357,65412,65456,65491,65515,65530,
-65535
-};
 
 void test() {
 
@@ -339,9 +319,7 @@ static double lookupSin(const double x) {
     index = 256 - index;
   }
 
-  // extended for the multiplication that is about to occur and keep the precision
-  uint32_t currentValue = sineTable[index];
-  double returnedValue = currentValue * 1.525902189669642175e-5; // divide by 65535
+  double returnedValue = sineTable[index] * 1.525902189669642175e-5; // divide by 65535
   return negativeFactor ? -returnedValue : returnedValue;
 }
 
@@ -354,7 +332,6 @@ static double lookupCos(const double x) {
   }
 
   long roundedAngle = lroundf(scaledAngle);  // This should be the _only_ line of FP code
-
   unsigned index = roundedAngle & (128 * 4 - 1);  // This & replaces fmod
 
   if (index >= 256) {
@@ -368,9 +345,6 @@ static double lookupCos(const double x) {
   }
 
   // extended for the multiplication that is about to occur and keep the precision
-  uint32_t currentValue;
-  currentValue = sineTable[128 - index]; 
-
-  double returnedValue = currentValue * 1.525902189669642175e-5; // divide by 65535
+  double returnedValue = sineTable[128 - index] * 1.525902189669642175e-5; // divide by 65535
   return negativeFactor ? -returnedValue : returnedValue;
 }
