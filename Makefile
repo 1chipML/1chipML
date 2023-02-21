b4247f346c4efa676ce07ef339b50100ff28b054
felix642 <60141446+felix642@users.noreply.github.com>
2023-02-21 12:40:05 -0500

    Implemented Lanczos Algorithm (#19)
    * Implemented Lanczos Algorithm
    
    The paper describing the algorithm can be found here : https://www.cs.umd.edu/~oleary/lanczos1950.pdf
    The exact implementation was based on the method proposed in the book "Numerical methods for large Eigenvalue Problems (ISBN 0-470-21820-7)
    This algorithm is prone to numerical instability. The Gram-Schmidt algorithm is used to reorthogonalize the vectors after each iteration.
    
    ---------
    
    Signed-off-by: Felix <felix-antoine.constantin@polymtl.ca>
    Signed-off-by: Florence Cloutier <florencecloutier@Florences-MBP.ht.home>

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@ CC=gcc
 
 CFLAGS += -g # adds debugging information
 CFLAGS += -Wall # turns on most compiler warnings
-CFLAGS += -std=c99 # C99
+CFLAGS += -std=c99# C99
 CFLAGS += -I./src # included header files
 
 # loaded libraries
 LDLIBS += -lm # Math library
 
-all: linear_congruential_random_generator gauss_elimination poly_interpolation DFT FFT monte_carlo
+all: linear_congruential_random_generator gauss_elimination poly_interpolation DFT FFT lanczos monte_carlo
 
 test: all run_all_tests
 
@@ -30,6 +30,10 @@ FFT: ./tests/test_FFT.c ./src/FFT.c ./src/DFT.c ./src/linear_congruential_random
 DFT: ./tests/test_DFT.c ./src/DFT.c ./src/linear_congruential_random_generator.c
 	$(CC) $(CFLAGS) $^ -o test_$@.out $(LDLIBS)
 
+lanczos: ./tests/test_lanczos.c ./src/lanczos.c ./src/linear_congruential_random_generator.c 
+	$(CC) $(CFLAGS) $^ -o test_$@.out $(LDLIBS)
+
+
 run_all_tests:
 	./test_linear_congruential_random_generator.out
 	./test_gauss_elimination.out
@@ -37,6 +41,7 @@ run_all_tests:
 	./test_monte_carlo.out
 	./test_DFT.out
 	./test_FFT.out
+	./test_lanczos.out
 
 clean:
 	rm -rf test_*.out
---
+++ src/lanczos.h
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+typedef double lanczos_real;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @input Matrix is the matrix for which you can to calculate the eigen values
+ * and eigen vectors
+ * @input dim is the size of the hermitian matrix
+ * @input nbIter is the number of iteration of the algorithm
+ * @input beginVector is a vector used for the first iteration of the algorithm.
+ * This parameter can be NULL
+ * @output tMatrix is the matrix T which can be used to compute eigen values and
+ * eigen vectors
+ * @output vMatrix is the matrix V which can be used to transforme eigen vectors
+ * from space of T to space or matrix
+ *
+ */
+void lanczos(lanczos_real* matrix, uint_least8_t dim, uint_least8_t nbIter,
+             lanczos_real* initialVector, lanczos_real* tMatrix,
+             lanczos_real* vMatrix);
+void vectorScale(lanczos_real* vector, uint_least8_t nbElements,
+                 lanczos_real scale);
+void vectorSubstract(lanczos_real* vector1, lanczos_real* vector2,
+                     uint_least8_t dim);
+void matrixMultiply(lanczos_real* firstMatrix, lanczos_real* secondMatrix,
+                    uint_least8_t size[3], lanczos_real* output);
+void makeUnitVector(lanczos_real* vector, uint_least8_t nbElements);
+#ifdef __cplusplus
+}
+#endif
