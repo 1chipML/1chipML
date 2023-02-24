a88fae45570d778ab851fffedd35dc8c08007895
Felix <felix-antoine.constantin@polymtl.ca>
2023-02-24 18:23:56 -0500

    Improved linear_congruential_random_generator This generator is a replacement for the rand() and random() functions which are quite slow on mcu. The rand() method requires ~850 cycles to return a value The first implementation of the linear_... needs 1.1k cycles. With those changes linear... can return a value after 250 cycles. Removing the multiplication brings down the latency to 16 cycles. But it involves changing the algorithms and this will need to be discussed first.
    Signed-off-by: Felix <felix-antoine.constantin@polymtl.ca>

--- src/linear_congruential_random_generator.c
+++ src/linear_congruential_random_generator.c
@@ -2,6 +2,8 @@
 
 #include "linear_congruential_random_generator.h"
 
+#include <stdint.h> 
+
 /* The routines below implements the linear congruential random
    number generator. In particular, the function
    "set_linear_congruential_generator_seed" sets the seed of the random
@@ -9,15 +11,15 @@
    returns a pseudo-random real number in the range [0, 1.0]. */
 
 /* Variables and pointers declarations */
-int ISEED = 16.; /* Default value in case the user does not specify the seed */
+uint16_t ISEED = 16; /* Default value in case the user does not specify the seed */
+#define LINEAR_RAND_MAX pow(2, sizeof(ISEED) * 8)
 
-/* Set the seed for the random number generator */
+
 void set_linear_congruential_generator_seed(int num) { ISEED = num; }
 
-/* Linear congruential random number generator */
 lcrg_real linear_congruential_random_generator(void) {
-  ISEED = fmod(75. * ISEED + 74., 65537.);
-  return (ISEED / 65537.);
+  ISEED = 75 * ISEED + 74;
+  return ISEED * (1.0 / LINEAR_RAND_MAX);
 }
 
 /* -- End of file -- */
