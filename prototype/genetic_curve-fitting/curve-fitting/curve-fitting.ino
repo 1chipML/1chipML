7b9c66ba389d3437c40f0bc46d4b56d2a5c3c169
nicholaslegrand102 <nicholaslegrand102@gmail.com>
2023-03-04 21:14:53 -0500

    Successul test
    Signed-off-by: nicholaslegrand102 <nicholaslegrand102@gmail.com>

--- prototype/genetic_curve-fitting/curve-fitting/curve-fitting.ino
+++ prototype/genetic_curve-fitting/curve-fitting/curve-fitting.ino
@@ -1,26 +1,27 @@
-#define genetic_real float
-#define genetic_int uint16_t
+
 #include "genetic.h"
 
 
 void setup() {
 
-}
+   Serial.begin(9600);
+  }
 
-    int coordinates[] = {169, 144, 121, 100, 81, 64,  49,  36,  25,
+int coordinates[] = {169, 144, 121, 100, 81, 64,  49,  36,  25,
                      16,  9,   4,   1,   0,  1,   4,   9,   16,
                      25,  36,  49,  64,  81, 100, 121, 144, 169};
 
     // int coordinates[] = {0 , 2.5, 6.5, 12.5, 20.5, 30.5, 42.5, 56.5, 72.5, 90.5,
     // 110.5, 132.5, 156.5, 182.5};
 
-  int currentIndex = 13;
+int currentIndex = 13;
   
-  float evaluationFunction(float *parameters) {
+float evaluationFunction(float *parameters) {
 
-  float fitness = 0;
+float fitness = 0;
 
-  for (int i = -currentIndex; i < currentIndex - 1; i++) {
+for (int i = -currentIndex; i < currentIndex - 1; i++) {
+
 
     fitness += fabs(parameters[0] * i + parameters[1] * pow(i, 2) +
                     parameters[2] * pow(i, 3) - coordinates[i + currentIndex] +
@@ -30,31 +31,36 @@ void setup() {
 }
 
 
-
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-    
-  unsigned int parameterCount = 4;
+  const unsigned int parameterCount = 4;
 
   float bestValues[parameterCount];
   float epsilon = 0.0001;
   float mutationRate = 1;
-  unsigned int populationSize = 100;
+  unsigned int populationSize =  50;
   unsigned int tourneySize = 10;
-  unsigned int maxIterations = 100000;
+  unsigned int maxIterations = 1000;
 
 
+
+ 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+
+  
   float value = geneticAlgorithm(bestValues, parameterCount, epsilon,
                                  mutationRate, populationSize, tourneySize,
-                                 maxIterations, evaluationFunction, 1);
-  printf("Value %f \n", value);
+                                 maxIterations, evaluationFunction, 0);
+  
+  Serial.println(value);
 
-  printf("Parameters %f %f %f %f \n", bestValues[0], bestValues[1],
-         bestValues[2], bestValues[3]);
-
-  exit(0)
+  Serial.println("Values ");
+  Serial.println(bestValues[0]);
+  Serial.println(bestValues[1]);
+  Serial.println(bestValues[2]);
+  Serial.println(bestValues[3]);
+  
+ 
 
 }
--- prototype/genetic_curve-fitting/curve-fitting/genetic.h
+++ prototype/genetic_curve-fitting/curve-fitting/genetic.h
@@ -10,18 +10,32 @@
 
 // We define a type for the fitness evaluation function that is passed in
 // parameters to the algorithm
-
+#ifndef genetic_real
 #define genetic_real float
+#endif
+
+#ifndef genetic_real_MAX_VALUE
 #define genetic_real_MAX_VALUE FLT_MAX
+#endif
 
 // We declare these constants to accelerate the execution of the algorithm
 
 #define INT_MAX_INVERSE (1.0 / UINT16_MAX)
-#define INT_MAX_DIGIT_COUNT 5
 
+#ifndef INT_MAX_DIGIT_COUNT
+#define INT_MAX_DIGIT_COUNT 5
+#endif
+
+#ifndef genetic_int
 #define genetic_int uint16_t
+#endif
 
 typedef genetic_real (*fitness_evaluation_function)(genetic_real *);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 genetic_real
 geneticAlgorithm(genetic_real *bestFitValues, const unsigned int parameterCount,
@@ -29,6 +43,10 @@ geneticAlgorithm(genetic_real *bestFitValues, const unsigned int parameterCount,
                  unsigned int generationSize, unsigned int tourneySize,
                  const unsigned int maximumIterationCount,
                  fitness_evaluation_function evaluationFunction,
-                 const unsigned int lowMemoryMode) {
+                 const unsigned int lowMemoryMode);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
