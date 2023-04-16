29c01bd2fedc29d9d1a8da0f034d2116a9ae564f
Florence Cloutier <florencecloutier@Florences-MBP.ht.home>
2023-04-15 21:54:58 -0400

    Added use of custom serial
    Signed-off-by: Florence Cloutier <florencecloutier@Florences-MBP.ht.home>

--- prototype/mc_shortest_path/arduino_mc_shortest_path/mc_shortest_path.py
+++ prototype/mc_shortest_path/arduino_mc_shortest_path/mc_shortest_path.py
@@ -1,29 +1,25 @@
-import struct
 import time
-import serial
+from serial_port import CustomSerial
 import numpy as np
 
 def main():
     size = 9
     boardValues = [-1, 10, 10, 1, 1, 10, 10, 1, 1]
+    nPlayers = 1
     lastIdx = 0
     
     np.set_printoptions(suppress=True) # Prevents from printing in scientific notation
     
-    port = serial.Serial('/dev/tty.usbmodem101', 9600)
+    port = CustomSerial('/dev/tty.usbmodem101', 9600)
     time.sleep(2) # Arduino takes time to initialize the serial connection
     
     while (True):
-        port.write(struct.pack('<B', size)) # Make sure that size in the arduino program is of 1 byte
-        
-        for value in boardValues:
-            port.write(struct.pack('<b', value))
+        port.writeElement('<B', size) # Make sure that size in the arduino program is of 1 byte
+        port.writeArray('<b', boardValues)
+        port.writeElement('<B', nPlayers)
 
-        nPlayers = 1
-        port.write(struct.pack('<B', nPlayers))
-
-        xPos = struct.unpack('<b', port.read(1))[0]
-        yPos = struct.unpack('<b', port.read(1))[0]
+        xPos = port.readElement( '<b', 1)
+        yPos = port.readElement('<b', 1)
 
         print("Best action: [", xPos, ",", yPos, "]")
         
@@ -33,7 +29,7 @@ def main():
         boardValues[xPos * 3 + yPos] = -1
         lastIdx = xPos * 3 + yPos
 
-    port.close()
+    port.closeSerial()
 
 if __name__ == "__main__":
     main()
